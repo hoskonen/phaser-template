09f2ec58c6046ab02130448440acc5cd0cec8209
Petri Hoskonen <petri.hoskonen@meteoriitti.com>
2020-01-29 15:48:28 +0200

    Initial commit.

---
+++ src/App.ts
@@ -0,0 +1,17 @@
+import * as Phaser from 'phaser';
+
+namespace App {
+
+  // game
+  export let game: Phaser.Game = null;
+}
+
+// -------------------------------------------------------------------------
+function launch(): void {
+
+  let game = new MaintainableGame.Game();
+  App.game = game;
+}
+
+// -------------------------------------------------------------------------
+window.onload = launch;
